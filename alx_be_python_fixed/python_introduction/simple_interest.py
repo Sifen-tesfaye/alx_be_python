4ce0858d9fb1cf741260fd90f0393cd112ef7501
Sifen Tesfaye Kumsa <sifen.tesfaye@aastustudent.edu.et>
2025-07-20 03:33:36 -0700

    Add weather_advice.py and clean up nested git repos

---
+++ alx_be_python_fixed/python_introduction/simple_interest.py
@@ -0,0 +1,10 @@
+# define variable
+principal = 1000
+rate = 0.05
+time = 3
+
+# calculate simpel interest 
+interest = principal * rate * time
+
+#print  the result 
+print(f"the simple interest is: {interest}")
