79563a1edcc345fef279c0c3d63f2764ea7ed684
Sifen tesfaye kumsa <sifen.tesfaye@aastustudent.edu.et>
2025-10-20 01:08:03 -0700

    updated simple interest  calculator

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,12 +1,7 @@
-def simple_interest(principal, rate_percent, time_years):
-   rate = rate_percent/100.0
-   interest = principal * rate * time_years
-   total = principal + interest
-   return interest, total 
-if __name__ == "__main__":   
-   p = float(input("principal: "))
-   r = float(input("Annual rate (%): "))
-   t = float(input("Time (years): "))
-   i, total = simple_interest(p, r, t)
-   print(f"Interest: {i : . 2f}")
-   print(f"Total amount : {total:.2f}")
+principal = 1000
+rate = 0.05
+time = 3
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}.")
